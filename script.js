d5920d22466345915f81769e5bf70d483160c7c5
Peter <peters1989@mail.ru>
2022-07-08 22:01:45 +0500

    Js начало

---
+++ script.js
@@ -0,0 +1,23 @@
+var name = prompt('Введите своё имя');
+console.log("Ваше имя " + name);
+
+
+var age = +prompt('Введите свой возраст');
+console.log('Ваш возраст ' + age);
+
+var addition = +prompt('Введите правельный ответ (17 + 22 = ?');
+console.log('Ваш ответ '+ addition + ' Правельный ответ ' + (17+22));
+
+var substraction = +prompt("Введите правельный ответ (48 - 14 = ?)");
+console.log('Ваш ответ ' + substraction + ' Правельный ответ ' + (48-14));
+
+var multiplication = +prompt('Введите правельный ответ 18 * 34 = ?');
+console.log('Ваш ответ ' + multiplication + ' Правельный ответ ' + (18 * 34));
+
+var devision = +prompt('Введите правельный ответ (18 / 3 = ?)');
+console.log('Ваш ответ ' + devision + " Правельный ответ " + (18 / 3));
+
+var devisionRemainder = +prompt('Вычислите остаток от деления (20 / 7 = ?');
+console.log( 'Ваш ответ ' + devisionRemainder + ' Правельный ответ ' + (20 / 7));
+
+alert('Откройте console для проверки результата');
